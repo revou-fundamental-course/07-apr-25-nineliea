3cd8a29b9e323210f500587314294d83762e014c
unknown <nicelyn.23007@gmail.com>
2025-04-16 23:59:27 +0700

    Header and Banner Done

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+window.addEventListener("load", () => {
+    document.body.classList.add("loaded");
+  });
+
 const savedInitial = localStorage.getItem("userInitial");
 if (savedInitial){
     document.getElementById("userInitial").innerHTML=savedInitial;
@@ -17,3 +21,25 @@ function validateForm () {
         return false;
     }
 }
+
+document.querySelectorAll('.nav-link').forEach(link => {
+    link.addEventListener('click', function (e) {
+      const href = this.getAttribute('href');
+  
+      if (href.startsWith('#')) {
+        e.preventDefault();
+        const target = document.querySelector(href);
+  
+        if (target) {
+          const offset = 100; // same as your scroll-padding-top
+          const topPos = target.offsetTop - offset;
+  
+          window.scrollTo({
+            top: topPos,
+            behavior: 'smooth'
+          });
+        }
+      }
+    });
+  });
+  
